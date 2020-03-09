000fff308d60915badcb85cf99df49085fda3db3
mrozi78 <mrozi.m@gmail.com>
2020-03-09 21:13:31 +0100

    Add some new examples

---
+++ Examples/Examples.Monads.Logic/Result.cs
@@ -0,0 +1,71 @@
+using System;
+
+namespace Examples.Monads.Logic
+{
+    public class Result
+    {
+        public bool IsSuccess { get; }
+        public string Error { get; }
+        public bool IsFailure => !IsSuccess;
+
+        protected Result(bool isSuccess, string error)
+        {
+            if (isSuccess && error != string.Empty)
+                throw new InvalidOperationException();
+            if (!isSuccess && error == string.Empty)
+                throw new InvalidOperationException();
+
+            IsSuccess = isSuccess;
+            Error = error;
+        }
+
+        public static Result Fail(string message)
+        {
+            return new Result(false, message);
+        }
+
+        public static Result<T> Fail<T>(string message)
+        {
+            return new Result<T>(default(T), false, message);
+        }
+
+        public static Result Ok()
+        {
+            return new Result(true, string.Empty);
+        }
+
+        public static Result<T> Ok<T>(T value)
+        {
+            return new Result<T>(value, true, string.Empty);
+        }
+        
+      
+    }
+
+
+    public class Result<T> : Result
+    {
+        private readonly T _value;
+        public T Value
+        {
+            get
+            {
+                if (!IsSuccess)
+                    throw new InvalidOperationException();
+
+                return _value;
+            }
+        }
+
+        protected internal Result(T value, bool isSuccess, string error)
+            : base(isSuccess, error)
+        {
+            _value = value;
+        }
+        public Result<TResult> Bind<TResult>(Func<T, Result<TResult>> func) where TResult : class
+        {
+            return func(_value);
+        }
+        
+    }
+}
